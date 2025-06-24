49e4ba64b90bea0d23f316f1cd3a41397868c02f
Charalambos Christou <c.charalambos1998@gmail.com>
2025-06-24 21:39:00 +0300

    Support multiple value filters.

--- LinqScribe/IQueryableExtensions/DynamicFiltersExtensions.cs
+++ LinqScribe/IQueryableExtensions/DynamicFiltersExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -5,20 +6,23 @@ namespace LinqScribe.IQueryableExtensions;
 
 public static class DynamicFiltersExtensions
 {
+    private const string ListPostfix = "List";
+    
     public static IQueryable<TSource> FilterWith<TSource, TFilters>(this IQueryable<TSource> source, TFilters filter) where TFilters : class
     {
         var parameter = Expression.Parameter(typeof(TSource), "p");
         var predicate = BuildExpression(typeof(TSource), filter, parameter);
-        var lambda = Expression.Lambda<Func<TSource, bool>>(predicate!, parameter);
+        if (predicate is null) return source;
+        var lambda = Expression.Lambda<Func<TSource, bool>>(predicate, parameter);
         return source.Where(lambda);
     }
     
-    private static BinaryExpression? BuildExpression<TFilters>(Type sourceType, TFilters filter, Expression parameter) where TFilters : class
+    private static Expression? BuildExpression<TFilters>(Type sourceType, TFilters filter, Expression parameter) where TFilters : class
     {
         var filterProperties = filter
             .GetType()
             .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-            .ToDictionary(k => k.Name);
+            .ToDictionary(k => RemoveListPostfixIfExists(k.Name));
 
         if (filterProperties.Count == 0)
             return null;
@@ -34,7 +38,7 @@ public static class DynamicFiltersExtensions
             return null;
         
         // "p" is going to be the parameter in our lambda expressions. E.g. (p => ...)
-        BinaryExpression? predicate = null;
+        Expression? predicate = null;
         foreach (var sourceProperty in sourceProperties)
         {
             object? value = filterProperties[sourceProperty.Name].GetValue(filter);
@@ -43,21 +47,50 @@ public static class DynamicFiltersExtensions
             
             var propertyAccess = Expression.Property(parameter, sourceProperty); // Access the property: "x.SourceProperty"
 
-            BinaryExpression? expression;
+            Expression? boolExpression;
             if (IsPrimitiveOrString(sourceProperty.PropertyType))
             {
-                expression = propertyAccess.EqualsTo(value);
+                boolExpression = BuildComparisonExpression(value, filterProperties[sourceProperty.Name].PropertyType, propertyAccess);
             }
-            else
+            else // For complex types (e.g., classes) iterate through its properties
             {
-                expression = BuildExpression(sourceProperty.PropertyType, value, propertyAccess);
+                boolExpression = BuildExpression(sourceProperty.PropertyType, value, propertyAccess);
             }
-            predicate = predicate == null ? expression : Expression.AndAlso(predicate, expression!);
+            predicate = predicate == null ? boolExpression : Expression.AndAlso(predicate, boolExpression!);
         }
 
         return predicate;
     }
+
+    private static Expression BuildComparisonExpression(object? value, Type propertyType, MemberExpression propertyAccess)
+    {
+        if (value is not string && value is IEnumerable values)
+        {
+            // Build constant expression for the list
+            var constantList = Expression.Constant(values);
+            
+            var containsMethod = propertyType
+                .GetMethods()
+                .FirstOrDefault(m => m.Name == nameof(Enumerable.Contains));
+            
+            if (containsMethod is null)
+            {
+                throw new InvalidOperationException($"Can't find `{nameof(Enumerable.Contains)}` method for {propertyType}");
+            }
+
+            // Build list.Contains(x.Property)
+            return Expression.Call(constantList, containsMethod, propertyAccess);
+        }
+        
+        // Build value == x.Property
+        return propertyAccess.EqualsTo(value);
+    }
     
     private static bool IsPrimitiveOrString(Type type) =>
-        type.IsPrimitive || type == typeof(string) || type == typeof(DateTime) || type == typeof(decimal);
+        type.IsPrimitive || type.IsValueType || type == typeof(string);
+    
+    private static string RemoveListPostfixIfExists(string name)
+    {
+        return name.Contains(ListPostfix) ? name.Replace(ListPostfix, string.Empty) : name;
+    } 
 }
---
+++ LinqScribeTests/TestDtos/MultipleValueFilter.cs
@@ -0,0 +1,7 @@
+namespace LinqScribeTests.TestDtos;
+
+public class MultipleValueFilter
+{
+    public List<string> EmailList { get; set; }
+    public List<int> IdList { get; set; }
+}
